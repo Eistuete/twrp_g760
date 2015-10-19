4e3864987e437fce28337741191a121a4cfa5230
Eistuete <vendor.hwg7@gmail.com>
2015-10-19 15:41:08 +0200

    first commit

---
+++ omni_g760.mk
@@ -0,0 +1,9 @@
+# Inherit device configuration
+$(call inherit-product, device/huawei/g760/device.mk)
+
+# Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := g760
+PRODUCT_NAME := omni_g760
+PRODUCT_BRAND := huawei
+PRODUCT_MANUFACTURER := huawei
+PRODUCT_MODEL := ASCEND G7
---
+++ vendorsetup.sh
@@ -0,0 +1,5 @@
+
+add_lunch_combo omni_g760-eng
+add_lunch_combo omni_g760-userdebug
+
+
